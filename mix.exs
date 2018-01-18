535c6401ec9dbc7b0f364d458d54d25c7b344090
haro-freezd <harmanmahal@gmail.com>
2018-01-17 22:18:06 -0800

    Updated dependecies

--- mix.exs
+++ mix.exs
@@ -14,13 +14,17 @@ defmodule Sitar.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      applications: [:sqlite_ecto, :ecto, :cowboy, :plug],
       extra_applications: [:logger]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
-  defp deps do
-    [
+  def deps do
+    [{:cowboy, "~> 1.0.0"},
+     {:plug, "~> 1.0"},
+     {:sqlite_ecto, "~> 1.0.0"},
+     {:ecto, "~> 1.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
